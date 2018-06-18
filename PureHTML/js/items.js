c814e7f5c85b16aedb6523fdf88c0e5c0b1e4b3e
Severin Mairinger <mai.sevi@hotmail.com>
2018-06-18 08:42:23 +0200

    asd
    asd

--- PureHTML/js/items.js
+++ PureHTML/js/items.js
@@ -18,14 +18,14 @@ function createItem(id, itemname, itemimage, cost, bought, wearing) {
     //Kaufen Button
 
     if (!bought) {
-        element += "<button style='margin-left: 1em; margin-top: 3.7em; position: absolute; height: 2em; background-color: limegreen; font-size: 2em;' class='MouseHover' id='btn_buy-"+ itemname +"' onclick='buy("+ cost +","+ itemname +")'> Kaufen </button>";
+        element += "<button style='margin-left: 1em; margin-top: 3.7em; position: absolute; height: 2em; background-color: #cdc600; font-size: 2em;' class='MouseHover' id='btn_buy-"+ itemname +"' onclick='buy("+ cost +","+ itemname +")'> Kaufen </button>";
     } else {
-        element += "<button style='margin-left: 1em; margin-top: 3.7em; position: absolute; height: 2em; background-color: #2c25aa; font-size: 2em;' class='MouseHover' id='btn_bought-"+ itemname +"'> Gekauft </button>";
+        element += "<button style='margin-left: 1em; margin-top: 3.7em; position: absolute; height: 2em; background-color: #22aa00; font-size: 2em;' class='MouseHover' id='btn_bought-"+ itemname +"'> Gekauft </button>";
         //Angezogen button
         if (!wearing) {
             element += "<button style='margin-left: 6em; margin-top: 3.7em; position: absolute; height: 2em; background-color: darkorange; font-size: 2em;' class='MouseHover' id='btn_wear-"+ itemname +"' onclick='wear(itemimage)' >Anziehen</button>";
         } else {
-            element += "<button style='margin-left: 6em; margin-top: 3.7em; position: absolute; height: 2em; background-color: #1f20ff; font-size: 2em;' class='MouseHover' id='btn_wearing-"+ itemname +"'>Angezogen</button>";
+            element += "<button style='margin-left: 6em; margin-top: 3.7em; position: absolute; height: 2em; background-color: #22aa00; font-size: 2em;' class='MouseHover' id='btn_wearing-"+ itemname +"'>Angezogen</button>";
         }
     }
 
